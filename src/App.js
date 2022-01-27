05a530233342b2bfb63f1e4a29fc3a72779dbeae
nekzconcepts <maziha.salih@gmail.com>
2022-01-27 22:45:31 +0530

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import "./App.css";
 import { Switch, Route } from "react-router-dom";
-import ShopPage from "./pages/shop/shop.component";
+
+import "./App.css";
+
 import HomePage from "./pages/homepage/homepage.component";
-import Header from "./components/header/header.component";
+import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import Header from "./components/header/header.component";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+
 class App extends React.Component {
   constructor() {
     super();
@@ -15,18 +18,31 @@ class App extends React.Component {
     };
   }
 
-  unsubscribeFronAuth = null;
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFronAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
   componentWillUnmount() {
-    this.unsubscribeFronAuth();
+    this.unsubscribeFromAuth();
   }
 
   render() {
